804637d7d310f820d1aadddb6c23a2cc8432fa58
Benjamin Nielsen <ben.a.nielsen@gmail.com>
2017-12-29 17:09:11 -0500

    Adds routing details and a concept image

--- ben-e-fits/src/app/app-routing.module.ts
+++ ben-e-fits/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
